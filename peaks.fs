61d9f9cd9f0a159e682d853ce1c4dc18f53c40d1
Lorenzo Mancini <lmancini@develer.com>
2012-09-17 09:14:26 +0200

    Cosmetics

--- peaks.fs
+++ peaks.fs
@@ -1,8 +1,10 @@
 uniform float blend;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
+
 varying vec4 color1;
 varying vec4 color2;
+
 void main(void)
 {
     vec4 color = color1 * blend + color2 * (1.0-blend);
